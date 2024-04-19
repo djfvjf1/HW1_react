47ce5e25baff7054c257c57351a4003e61ecf897
djfvjf <mih2864@mail.ru>
2024-04-19 19:18:17 +0500

    6 commit

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,14 +1,32 @@
 import React from "react";
 import TodoListItem from "./todo-list-item";
 
-const TodoList = () => {
+const TodoList = ({ todos}) => {
 
     return (
         <ul>
-            <li><TodoListItem /></li>
-            <li><TodoListItem /></li>
+           
+            <li>
+                <TodoListItem
+                    label = {todos[0].label}
+                    important={todos[0].important} />
+            </li>
+            <li>
+                <TodoListItem
+                    label = {todos[1].label}
+                    important = {todos[1].important} />
+            </li>
+            <li>
+                <TodoListItem
+                    label = {todos[2].label}
+                    important = {todos[2].important} />
+            </li>
+            <li>
+                <TodoListItem
+                    label = {todos[3].label}
+                    important = {todos[3].important} />
+            </li>
         </ul>
     );
 };
-
 export default TodoList;
--- src/index.js
+++ src/index.js
@@ -4,18 +4,22 @@ import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
 
-
 const App = () => {
 
-  return (
-      <div>
-          <AppHeader />
-          <SearchPanel />
-          <TodoList/>
-      </div>
-  );
+    const todoData = [
+        { label: 'Drink Coffee', important: false },
+        { label: 'Implement React Application', important: true },
+        { label: 'Make notes from your stud', important: false },
+        { label: 'Do not drink alchohol', important: false },
+    ];
+
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList todos =  {todoData} />
+        </div>
+    );
 }
-
-
-  ReactDOM.render(<App />,
-      document.getElementById('root'));
+    ReactDOM.render(<App />,
+        document.getElementById('root'));
